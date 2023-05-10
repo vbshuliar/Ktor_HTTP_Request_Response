e89edb107d0a3fd955c8b346b6b7e655466192db
vbshuliar <vbshuliar@gmail.com>
2023-05-10 17:22:32 +0100

    feat: all done

--- app/src/main/java/com/example/littlelemon/MainActivity.kt
+++ app/src/main/java/com/example/littlelemon/MainActivity.kt
@@ -8,19 +8,20 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Button
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.lifecycleScope
 import com.example.littlelemon.ui.theme.LittleLemonTheme
-import com.google.firebase.crashlytics.buildtools.reloc.org.apache.http.HttpResponse
-import com.google.firebase.crashlytics.buildtools.reloc.org.apache.http.client.HttpClient
+import io.ktor.client.HttpClient
 import io.ktor.client.engine.android.Android
+import io.ktor.client.request.get
+import io.ktor.client.statement.bodyAsText
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     private val responseLiveData = MutableLiveData<String>()
     private val httpClient = HttpClient(Android)
-    private suspend fun fetchContent(url: String): HttpResponse {
-        return httpClient.get("https://raw.githubusercontent.com/Meta-Mobile-Developer-PC/Working-With-Data-API/main/littleLemonMenu.json")
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,18 +30,29 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize()
                 ) {
-                    var responseState = responseLiveData.value
+                    val responseState = responseLiveData.observeAsState("").value
                     Column {
                         Button(
                             onClick = {
-
+                                lifecycleScope.launch {
+                                    val response = fetchContent()
+                                    runOnUiThread {
+                                        responseLiveData.value = response
+                                    }
+                                }
                             }
                         ) {
                             Text(text = "Download")
                         }
+                        Text(text = responseState.toString())
                     }
                 }
             }
         }
     }
+
+    private suspend fun fetchContent(): String {
+        return httpClient.get("https://raw.githubusercontent.com/Meta-Mobile-Developer-PC/Working-With-Data-API/main/littleLemonMenu.json").bodyAsText()
+    }
+
 }
